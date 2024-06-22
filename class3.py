949564b028fcef502dc1388c912e2a14606b9538
amirhossain1993 <amir375413@gmail.com>
2024-06-22 19:07:12 +0600

    update

---
+++ class3.py
@@ -0,0 +1,137 @@
+# Data Type
+'''
+# List type data - note: [] = Square Brackets or Box Brackets use
+x = ["Mango" , "Orange" , "Cherry"]
+print(type(x))
+print(x)
+'''
+
+
+'''
+# Tuple type data - note: () = Parentheses or Round Bracket use
+x = ("Tiger" , "Lion" , "Monky")
+print(type(x))
+print(x)
+'''
+
+
+'''
+# Dictionary Data - note: {} = Braces or Curly Brackets use
+x = {"name" : "Jamal" , "age" : 32}
+print(type(x))
+print(x)
+'''
+
+
+'''
+# set type data -  note: {} = Braces or Curly Brackets use
+x = {"Monir" , "Yeasin" , "Alomgir"}
+print(type(x))
+print(x)
+'''
+
+
+'''
+# Frozenset type data - note: {} & () = Braces or Curly Brackets & Parentheses or Round Bracket use
+x = frozenset({"Apple" , "Banana" , "Orange"})
+print(type(x))
+print(x)
+'''
+
+
+'''
+# Boolean type data - conditional True / false
+x = True
+print(type(x))
+print(x)
+'''
+
+'''
+# None type data
+x = None
+print(type(x))
+print(x)
+'''
+
+
+# Arithmetic Operators
+
+'''
+x = 21
+y = 2
+
+print(x + y)   # Addition
+print(x - y)   # Subtraction
+print(x * y)   # Multiplication
+print(x / y)   # Division
+print(x % y)   # Modulus - note vag sas dakhabe
+'''
+
+
+'''
+# Exponentiation : power valu (**)
+x = 3
+y = 2
+print(x ** y)   # Exponentiation - note: x er valu er upor y er valu power hesabe kaj korbe
+'''
+
+'''
+# Floor division : (//)
+x = 19
+y = 5
+print(x / y)   # Division - note: dosomic valu print korbe 
+print(x // y)  # Floor division - note: dosomic er ager purno valu print korbe
+'''
+
+
+
+# Assignment Operator
+'''
+=            x = 5              x = 5
++=           x += 3             x = x + 3
+-=           x -= 3             x = x - 3
+*=           x *= 3             x = x * 3
+/=           x /= 3             x = x / 3
+%=           x %= 3             x = x % 3
+//=          x //= 3            x = x // 3
+**=          x **= 3            x = x ** 3
+&=           x &= 3             x = x & 3
+|=           x |= 3             x = x | 3
+^=           x ^= 3             x = x ^ 3
+>>=          x >>= 3            x = x >> 3
+<<=          x <<= 3            x = x << 3
+:=           print(x := 3)      x = 3
+                                print(x)
+'''
+
+'''
+# (+=)
+# Example: 01
+x = 1
+while x < 10:
+    print(x)
+    x += 1
+
+# Example: 02
+x = 1
+while x < 10:
+    print(x)
+    x += 2
+'''
+
+'''
+# (-=) not working  : show the W3 school wedsite
+# Example: 01
+x = 11
+while x < 10:
+    print(x)
+    x -= 1
+''' 
+
+    
+
+  
+
+
+    
+
---
+++ class4.py
@@ -0,0 +1,70 @@
+'''
+# The Largest Value :
+
+num1= 10
+num2= 20
+num3= 15
+if num1 >= num2 and num1 >= num3:
+    larges = num1
+elif num2 >= num1 and num2 >= num3:
+    largest = num2
+else:
+    largest = num3
+    
+print("The Largest Number is :", largest)
+'''
+
+
+# Using Loops (while and for)
+'''
+i = 1
+while i <10:
+    print(i, "I Love Bangladesh")
+    i +=1
+'''
+'''
+i = 1
+while i <10:
+    print(i, "Abdullah Al Ahnaf")
+    i += 2
+'''
+
+
+'''
+# Even And Odd Number Using while loop home work
+
+i = 1
+while i < 10:
+    print(i)
+    i += 2     # Odd Number
+    
+
+i = 2
+while i < 10:
+    print(i)
+    i += 2     # Even Number
+'''
+
+
+
+# break and continue command
+
+'''
+i = 1
+while i < 10:
+    print(i)
+    if i == 7:
+        break    # i er valu 7 asa matroi break kore dise
+    i += 1
+'''
+'''
+i = 0
+while i < 10:
+    i += 1
+    if i == 7:
+        continue
+    print(i)
+'''
+
+
+
